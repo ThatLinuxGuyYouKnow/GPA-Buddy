88471a8e2b37a3dd03d0f48d593718efa53b048a
Emmanuels-code <arocket04@gmail.com>
2024-11-27 15:56:55 +0100

    added google fonts

--- lib/gpa.dart
+++ lib/gpa.dart
@@ -1,24 +1,26 @@
 import 'package:flutter/material.dart';
 
-class GpaScreen extends StatefulWidget {
+class GpaScreen extends StatelessWidget {
   GpaScreen({super.key});
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('GPA Calculator'),
-      ),
-      body: LayoutBuilder(
-        builder: (BuildContext context, BoxConstraints constraints) {
-          final isDesktop = constraints.maxWidth > 900;
-          final isTablet =
-              constraints.maxWidth > 600 && constraints.maxWidth <= 900;
-          final containerWidth = isDesktop
-              ? constraints.maxWidth * 0.4
-              : isTablet
-                  ? constraints.maxWidth * 0.6
-                  : constraints.maxWidth * 0.85;
-        },
-      ),
+    return LayoutBuilder(
+      builder: (BuildContext context, BoxConstraints constraints) {
+        final isDesktop = constraints.maxWidth > 900;
+        final isTablet =
+            constraints.maxWidth > 600 && constraints.maxWidth <= 900;
+        final containerWidth = isDesktop
+            ? constraints.maxWidth * 0.4
+            : isTablet
+                ? constraints.maxWidth * 0.6
+                : constraints.maxWidth * 0.85;
+        return Scaffold(
+          body: Center(
+            child: Column(
+              children: [],
+            ),
+          ),
+        );
+      },
     );
   }
 }

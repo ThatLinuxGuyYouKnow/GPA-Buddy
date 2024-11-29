fd3ae02cfedd002dc250804048e0d4cfd9c5a88a
Emmanuels-code <arocket04@gmail.com>
2024-11-29 01:05:39 +0100

    feature: minimal logging in GPA calculator

--- lib/logic/finalGpaCalculator.dart
+++ lib/logic/finalGpaCalculator.dart
@@ -1,7 +1,7 @@
 calculateGPA(
     {required double totalGradeWeights, required double numberOfCourses}) {
   print(numberOfCourses.toString() + "  number of courses");
-  print(totalGradeWeights.toString() + "  number of courses");
+  print(totalGradeWeights.toString() + "  grade weights");
   final double gpa = totalGradeWeights / numberOfCourses;
   print(gpa);
   return totalGradeWeights / numberOfCourses;

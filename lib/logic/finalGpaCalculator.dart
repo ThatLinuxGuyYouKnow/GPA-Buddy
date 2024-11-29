ce6c33c35598c15feb0699bf988891203adcf4ac
Emmanuels-code <arocket04@gmail.com>
2024-11-29 14:23:36 +0100

    refactor: reworked calculate GPA logic

--- lib/logic/finalGpaCalculator.dart
+++ lib/logic/finalGpaCalculator.dart
@@ -1,7 +1,8 @@
 double calculateGPA(
     {required double totalGradeWeights, required int totalUnits}) {
-  print(totalUnits.toString() + "         total units");
   print(totalGradeWeights.toString() + "        totalGradeWeightss");
+  print(totalUnits.toString() + "         total units");
+
   if (totalUnits == 0) return 0.0; // Avoid division by zero
   return totalGradeWeights / totalUnits;
 }

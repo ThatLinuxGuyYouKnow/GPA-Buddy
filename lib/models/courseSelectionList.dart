e479161f06f71f78513e025e634b0bf3e6d4ea68
Emmanuels-code <arocket04@gmail.com>
2024-11-30 00:53:01 +0100

    refactor! : added provider

--- lib/models/courseSelectionList.dart
+++ lib/models/courseSelectionList.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:gpa_calculator/widgets/courseSection.dart';
 
 class CourseSelectionList extends ChangeNotifier {
-  final List<Widget> courses = [];
+  final List<Widget> courses = [CourseSubsection()];
   List<Widget> get courseList => courses;
+
+  addCourse() {
+    courses.add(const CourseSubsection());
+  }
+
+  removeCourse() {
+    courses.removeLast();
+  }
 }

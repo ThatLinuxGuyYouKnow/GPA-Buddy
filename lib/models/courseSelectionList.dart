40d22aceaa4679cb1aac13308553168d24717cd1
Emmanuels-code <arocket04@gmail.com>
2024-11-30 01:54:44 +0100

    refactor! : moving data state to Provider class

--- lib/models/courseSelectionList.dart
+++ lib/models/courseSelectionList.dart
@@ -4,7 +4,7 @@ import 'package:gpa_calculator/widgets/courseSection.dart';
 class CourseSelectionList extends ChangeNotifier {
   final List<Widget> courses = [const CourseSubsection()];
   List<Widget> get courseList => courses;
-  List<int> courseWeight = [];
+  List<int> get courseWeight => _courseWeight;
   List<int> _courseWeight = [];
   addCourse() {
     courses.add(const CourseSubsection());
@@ -16,5 +16,7 @@ class CourseSelectionList extends ChangeNotifier {
     notifyListeners();
   }
 
-  updateCourseUnit(int index, String text) {}
+  updateCourseUnit(int index, String text) {
+    _courseWeight[index];
+  }
 }

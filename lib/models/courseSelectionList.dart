78def6b8c53b3f5f6064e5e8700e0c948624ae93
Emmanuels-code <arocket04@gmail.com>
2024-11-30 01:30:45 +0100

    refactor! : moving data state to Provider class

--- lib/models/courseSelectionList.dart
+++ lib/models/courseSelectionList.dart
@@ -2,14 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:gpa_calculator/widgets/courseSection.dart';
 
 class CourseSelectionList extends ChangeNotifier {
-  final List<Widget> courses = [CourseSubsection()];
+  final List<Widget> courses = [const CourseSubsection()];
   List<Widget> get courseList => courses;
 
   addCourse() {
     courses.add(const CourseSubsection());
+    notifyListeners();
   }
 
   removeCourse() {
     courses.removeLast();
+    notifyListeners();
   }
 }

35f0c4ce2482ef617dc687224346cef028629151
Emmanuels-code <arocket04@gmail.com>
2024-11-30 01:57:44 +0100

    refactor! : registered Course provider to app

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:gpa_calculator/gpa.dart';
+import 'package:gpa_calculator/models/courseSelectionList.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(ChangeNotifierProvider(
+      create: (context) => CourseSelectionList(), child: const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
